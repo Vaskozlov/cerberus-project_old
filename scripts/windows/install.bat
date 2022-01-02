8aad28638fa52ceebc2ac9d581ec58d2ffbaa8af
vaskozlov <vaskozlov3940@gmail.com>
2022-01-02 17:39:08 +0500

    new way to install cmake for windows

--- scripts/windows/install.bat
+++ scripts/windows/install.bat
@@ -7,7 +7,7 @@ if not exist "%tempPath%\tmp"      mkdir "%tempPath%\tmp"
 cd "%tempPath%"
 set extra_path=\vcpkg\scripts\buildsystems\vcpkg.cmake
 set CMAKE_TOOLCHAIN_FILE=%cd%%extra_path%
-set cmake_dir=%cd%\cmake\cmake-3.22.0-rc3-windows-x86_64\bin
+set cmake_dir=C:\Program Files\CMake\bin
 
 if not exist ".\cmake"      mkdir ".\cmake"
 
@@ -23,7 +23,6 @@ cd ..\..\..\
 mkdir build
 cd build
 powershell -Command "%cmake_dir%\cmake.exe -DCMAKE_BUILD_TYPE=Release -G 'Visual Studio 16 2019' -A x64 -S .. -DCMAKE_TOOLCHAIN_FILE=%CMAKE_TOOLCHAIN_FILE%"
-powershell -Command "%cmake_dir%\ctest.exe ."
 cd ..
 
 echo "SUCCESS! Installation is completed"
