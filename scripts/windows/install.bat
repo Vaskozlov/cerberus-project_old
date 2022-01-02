0f34dcb2ef119107c7bf3c3cccd446777bca1ddd
vaskozlov <vaskozlov3940@gmail.com>
2022-01-02 20:59:58 +0500

    fix

--- scripts/windows/install.bat
+++ scripts/windows/install.bat
@@ -22,7 +22,7 @@ cd ..\..\..\
 
 mkdir build
 
-powershell -Command "%cmake_dir%\cmake.exe\" -DCMAKE_BUILD_TYPE=Release -G 'Visual Studio 16 2019' -A x64 -S .. -DCMAKE_TOOLCHAIN_FILE=%CMAKE_TOOLCHAIN_FILE%"
+powershell -Command "%cmake_dir%\cmake.exe  " -DCMAKE_BUILD_TYPE=Release -G 'Visual Studio 16 2019' -A x64 -S .. -DCMAKE_TOOLCHAIN_FILE=%CMAKE_TOOLCHAIN_FILE%"
 
 echo "SUCCESS! Installation is completed"
 
