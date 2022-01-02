10fcc24f72dccf8194183ee8ccc96e5316294344
vaskozlov <vaskozlov3940@gmail.com>
2022-01-02 20:39:44 +0500

    new attempt

--- scripts/windows/install.bat
+++ scripts/windows/install.bat
@@ -7,7 +7,7 @@ if not exist "%tempPath%\tmp"      mkdir "%tempPath%\tmp"
 cd "%tempPath%"
 set extra_path=\vcpkg\scripts\buildsystems\vcpkg.cmake
 set CMAKE_TOOLCHAIN_FILE=%cd%%extra_path%
-set cmake_dir=C:\Program Files\CMake\bin
+set cmake_dir=C:\"Program Files"\CMake\bin
 
 if not exist ".\cmake"      mkdir ".\cmake"
 
@@ -22,7 +22,7 @@ cd ..\..\..\
 
 mkdir build
 
-powershell -Command "\"%cmake_dir%\cmake.exe\" -DCMAKE_BUILD_TYPE=Release -G 'Visual Studio 16 2019' -A x64 -S .. -DCMAKE_TOOLCHAIN_FILE=%CMAKE_TOOLCHAIN_FILE%"
+powershell -Command "%cmake_dir%\cmake.exe\" -DCMAKE_BUILD_TYPE=Release -G 'Visual Studio 16 2019' -A x64 -S .. -DCMAKE_TOOLCHAIN_FILE=%CMAKE_TOOLCHAIN_FILE%"
 
 echo "SUCCESS! Installation is completed"
 
