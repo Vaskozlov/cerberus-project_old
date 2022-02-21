120c1b4517d61d0730872b28fa09063151f3aba3
vaskozlov <vaskozlov3940@gmail.com>
2022-02-21 20:24:22 +0500

    File for debugging and generating random arrays

---
+++ libcerberus/include/cerberus/debug/debug.hpp
@@ -0,0 +1,52 @@
+#ifndef LIBCERBERUS_CATCH_CATCH_HPP
+#define LIBCERBERUS_CATCH_CATCH_HPP
+
+#include <cerberus/cerberus.hpp>
+#include <cerberus/exception.hpp>
+#include <cerberus/location.hpp>
+#include <cerberus/pair.hpp>
+#include <fmt/color.h>
+#include <fmt/format.h>
+#include <iostream>
+
+#define EXPECT_TRUE(value) expectTrue(value, CERBLIB_LOCATION)
+#define EXPECT_FALSE(value) expectFalse(value, CERBLIB_LOCATION)
+
+namespace cerb::debug
+{
+    CERBERUS_EXCEPTION(RuntimeError);
+    CERBERUS_EXCEPTION(CompileTimeError);
+
+    using PairedNumbers = Pair<ssize_t, double>;
+
+    inline auto failure(bool condition, Location const &loc = CERBLIB_LOCATION) -> void
+    {
+        fmt::print(fmt::fg(fmt::color::red), "Cerberus test failure with code: {}! ", condition);
+        fmt::print("File: {}, getLine: {}\n", loc.getFilename(), loc.getLine());
+        throw RuntimeError();
+    }
+
+    constexpr auto expectTrue(bool condition, Location const &loc = CERBLIB_LOCATION) -> void
+    {
+        if (not condition) {
+            if (std::is_constant_evaluated()) {
+                throw CompileTimeError("Cerberus test failure!");
+            } else {
+                failure(condition, loc);
+            }
+        }
+    }
+
+    constexpr auto expectFalse(bool condition, Location const &loc = CERBLIB_LOCATION) -> void
+    {
+        if (condition) {
+            if (std::is_constant_evaluated()) {
+                throw CompileTimeError("Cerberus test failure!");
+            } else {
+                failure(condition, loc);
+            }
+        }
+    }
+}// namespace cerb::test
+
+#endif /* LIBCERBERUS_CATCH_CATCH_HPP */
