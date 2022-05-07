8bfc27fba1ca90f7d5e1167aec1f3b0091eb7802
vaskzolov <vaskozlov3940@gmail.com>
2022-05-07 21:28:42 +0500

    Code style fix №2

--- analysis/catch/lexical/lexical_analyzer.cpp
+++ analysis/catch/lexical/lexical_analyzer.cpp
@@ -1,11 +1,13 @@
 #include <cerberus/debug/debug.hpp>
 #include <cerberus/lex/lexical_analyzer.hpp>
 
-namespace cerb::debug {
+namespace cerb::debug
+{
     using namespace lex;
 
-    auto testLexicalAnalyzer() -> int {
-        CERBLIB_UNUSED(LexicalAnalyzer<char>) = {{0, "[0-9]+p\"for\""}};
+    auto testLexicalAnalyzer() -> int
+    {
+        CERBLIB_UNUSED(LexicalAnalyzer<char>) = { { 0, "[0-9]+p\"for\"" } };
         return 0;
     }
 }// namespace cerb::debug
